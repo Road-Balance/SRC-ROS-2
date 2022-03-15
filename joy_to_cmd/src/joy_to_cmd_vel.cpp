a112ac976ebb8f9112e69737fbb53f8bd3488172
kimsooyoung <tge1375@naver.com>
2022-03-15 13:19:51 +0900

    Update : accel & max speed control

--- joy_to_cmd/src/joy_to_cmd_vel.cpp
+++ joy_to_cmd/src/joy_to_cmd_vel.cpp
@@ -3,10 +3,12 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int8.hpp"
+#include "std_msgs/msg/float32.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
 using UInt8 = std_msgs::msg::UInt8;
+using Float32 = std_msgs::msg::Float32;
 using Joy = sensor_msgs::msg::Joy;
 using Twist = geometry_msgs::msg::Twist;
 
@@ -26,6 +28,8 @@ public:
     float right_updown;
     float right_leftright;
 
+    int btn_updown;
+
     bool btn_a;
     bool btn_b;
     bool btn_x;
@@ -41,24 +45,38 @@ public:
 private:
   rclcpp::Publisher<Twist>::SharedPtr cmd_vel_pub;
   rclcpp::Publisher<UInt8>::SharedPtr src_mode_pub;
+  rclcpp::Publisher<Float32>::SharedPtr accel_pub;
+
   rclcpp::Subscription<Joy>::SharedPtr joy_sub;
 
   Twist twist;
   UInt8 src_mode;
+  Float32 accel;
 
+  XMode prev_joy_keys;
   XMode joy_keys;
+
+
+  float linear_speed_gain = 1.0;
+  const float angular_pose_gain = 2.0;
 
 public:
   JoyToCmd() : Node("joy_to_cmd_vel_node")
   {
-
+    // TODO : QOS Profile
     cmd_vel_pub = create_publisher<Twist>("cmd_vel", 5);
     src_mode_pub = create_publisher<UInt8>("src_mode", 5);
+    accel_pub = create_publisher<Float32>("accel_vel", 5);
 
     joy_sub = create_subscription<Joy>(
       "joy", 10,
       std::bind(&JoyToCmd::sub_callback, this, std::placeholders::_1)
     );
+
+    twist.linear.x = 0.0;
+    twist.angular.z = 0.0;
+
+    accel.data = 0.75f;
 
     src_mode.data = 1;
   }
@@ -71,34 +89,49 @@ public:
     joy_keys.right_updown = data->axes[4];
     joy_keys.right_leftright = data->axes[3];
 
+    // +1 btn up / -1 btn down
+    joy_keys.btn_updown = data->axes[7];
+
+    // buttons for mode change
     joy_keys.btn_a = isTrue(data->buttons[0]);
     joy_keys.btn_b = isTrue(data->buttons[1]);
     joy_keys.btn_x = isTrue(data->buttons[2]);
     joy_keys.btn_y = isTrue(data->buttons[3]);
 
+    // Linear Speed UP/Down
     joy_keys.btn_LB = isTrue(data->buttons[4]);
     joy_keys.btn_RB = isTrue(data->buttons[5]);
 
     joy_keys.btn_back = isTrue(data->buttons[6]);
     joy_keys.btn_start = isTrue(data->buttons[7]);
 
-    calc_cmd_vel();
+
+    pub_twist();
     pub_mode();
+    pub_accel();
+
+    prev_joy_keys = joy_keys;
   }
 
-  void calc_cmd_vel()
+  void pub_twist()
   {
-    twist.linear.x = joy_keys.left_updown;
-    twist.linear.y = joy_keys.left_leftright;
-    twist.linear.z = 0.0;
+    // floating point number considered
+    if (joy_keys.btn_LB && !prev_joy_keys.btn_LB)
+      linear_speed_gain -= 0.125f;
+    if (joy_keys.btn_RB && !prev_joy_keys.btn_RB)
+      linear_speed_gain += 0.125f;
 
-    // TODO: Orientation
-    twist.angular.z = joy_keys.right_leftright * 2;
+    twist.linear.x = joy_keys.left_updown * linear_speed_gain;
+    twist.linear.y = joy_keys.left_leftright;
+    twist.linear.z = 0.0f;
+
+    twist.angular.z = joy_keys.right_leftright * angular_pose_gain;
 
     cmd_vel_pub->publish(twist);
   }
-  
+
   void pub_mode(){
+    // Emergency Stop
     if (joy_keys.btn_x)
       src_mode.data = 1;
     if (joy_keys.btn_a)
@@ -109,6 +142,15 @@ public:
       src_mode.data = 4;
 
     src_mode_pub->publish(src_mode);
+  }
+
+  void pub_accel(){
+    if (joy_keys.btn_updown == 1.0 && prev_joy_keys.btn_updown == 0.0)
+      accel.data += 0.125f;
+    if (joy_keys.btn_updown == -1.0 && prev_joy_keys.btn_updown == 0.0)
+      accel.data -= 0.125f;
+
+    accel_pub->publish(accel);
   }
 };
 
