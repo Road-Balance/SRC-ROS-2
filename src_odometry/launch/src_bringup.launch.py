4a5b3afbe88c8d4e00f083fb39cd5ed86637e9f1
kimsooyoung <tge1375@naver.com>
2022-04-22 23:37:28 +0900

    Update : bring up for slam & etc

--- src_odometry/launch/src_bringup.launch.py
+++ src_odometry/launch/src_bringup.launch.py
@@ -41,7 +41,21 @@ def generate_launch_description():
         }],
     )
 
+    cmd_to_src = Node(
+        package='cmd_to_src',
+        executable='cmd_to_src',
+        name='cmd_to_src',
+        output='screen',
+        parameters=[{
+            "p_gain" : 122.0,
+            'i_gain' : 3.0,
+            'd_gain' : 1.0,
+            'use_twiddle' : False,
+        }],
+    )
+
     return LaunchDescription([
         mw_ahrs_node,
         src_odom,
+        cmd_to_src,
     ])
