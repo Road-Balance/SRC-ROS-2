646e22f077ed39a955b900873be381f5239b0b40
kimsooyoung <tge1375@naver.com>
2022-05-05 22:53:11 +0900

    Ongoing : PID tuning

--- cmd_to_src/launch/cmd_to_src.launch.py
+++ cmd_to_src/launch/cmd_to_src.launch.py
@@ -12,12 +12,17 @@ def generate_launch_description():
         output='screen',
         parameters=[{
             "accel_scale" : 0.1,
-            "deaccel_scale" : 0.1,
-            "scale" : 24, # linear 하지 못하다. 0.1에서는 좀 못미치고, 0.4에서는 넘어버린다.
+            "deaccel_scale" : 0.5,
+
+            "scale" : 40, # linear 하지 못하다. 0.1에서는 좀 못미치고, 0.4에서는 넘어버린다.
+            # "p_gain" : 145.0,
+            # "i_gain" : 3.0,
+            # "d_gain" : 0.0,
+
             "p_gain" : 122.0,
-            'i_gain' : 3.0,
-            'd_gain' : 1.0,
-            'use_twiddle' : False,
+            "i_gain" : 3.0,
+            "d_gain" : 1.0,
+            "use_twiddle" : False,
         }],
     )
 
