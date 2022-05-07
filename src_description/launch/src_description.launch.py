4453c13782453ade43c3ebb844ac4516330d753c
kimsooyoung <tge1375@naver.com>
2022-05-07 23:35:10 +0900

    Update : urdf naming changed

--- src_description/launch/src_description.launch.py
+++ src_description/launch/src_description.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
 
     pkg_path = os.path.join(get_package_share_directory("src_description"))
     rviz_config_file = os.path.join(pkg_path, "rviz", "description.rviz")
-    xacro_file = os.path.join(pkg_path, "urdf", "racecar.urdf.xacro")
+    xacro_file = os.path.join(pkg_path, "urdf", "src_ackermann.urdf.xacro")
 
     # Get URDF via xacro
     robot_description_content = Command(
