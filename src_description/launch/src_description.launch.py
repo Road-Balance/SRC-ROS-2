09c13e8fddb3247d476a6e071865eea99ab3fd9a
kimsooyoung <tge1375@naver.com>
2023-06-25 19:57:17 +0900

    desc updated

--- src_description/launch/src_description.launch.py
+++ src_description/launch/src_description.launch.py
@@ -1,26 +1,9 @@
-#!/usr/bin/env python3
-#
-# Copyright 2022 RoadBalance Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import TimerAction
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
 
 from launch.substitutions import (
     Command,
@@ -32,26 +15,7 @@ def generate_launch_description():
 
     pkg_path = os.path.join(get_package_share_directory("src_description"))
     rviz_config_file = os.path.join(pkg_path, "rviz", "description.rviz")
-    xacro_file = os.path.join(pkg_path, "urdf", "src_ackermann.urdf.xacro")
-
-    # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            xacro_file,
-            " ",
-            "prefix:=",
-            '""',
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
-
-    joint_state_publisher = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        name="joint_state_publisher",
-    )
+    urdf_file = os.path.join(pkg_path, "urdf", "src_body.urdf")
 
     joint_state_publisher_gui = Node(
         package="joint_state_publisher_gui",
@@ -60,14 +24,16 @@ def generate_launch_description():
     )
 
     robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[robot_description],
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': True}],
+        arguments=[urdf_file],
     )
 
     # Launch RViz
-    rviz = Node(
+    rviz2 = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
@@ -78,9 +44,9 @@ def generate_launch_description():
     return LaunchDescription([
         TimerAction(
             period=3.0,
-            actions=[rviz]
+            actions=[rviz2]
         ),
-        joint_state_publisher, 
-        robot_state_publisher, 
-        joint_state_publisher_gui, 
-    ])  
+
+        robot_state_publisher,
+        joint_state_publisher_gui,
+    ])
